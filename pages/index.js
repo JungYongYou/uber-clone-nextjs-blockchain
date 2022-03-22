2e56c642b49b8dc5d8f85abbd8d8f25038dc9e8d
jydeveloper <lukaryu@naver.com>
2022-03-22 15:34:47 +0900

    LocationSelector 추가

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Navbar from '../components/Navbar';
 import Map from '../components/Map';
+import LocationSelector from '../components/LocationSelector';
 
 const style = {
 	wrapper: `h-screen w-screen flex flex-col`,
 	main: `h-full w-screen flex-1 z-10`,
 	mapContainer: `flex-1 w-fulil h-full`,
-	rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end`,
+	rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
 	rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 };
 
@@ -18,7 +19,7 @@ export default function Home() {
 			</div>
 			<div className={style.rideRequestContainer}>
 				<div className={style.rideRequest}>
-					{/* location selector */}
+					<LocationSelector />
 					{/* confirm ride */}
 				</div>
 			</div>
